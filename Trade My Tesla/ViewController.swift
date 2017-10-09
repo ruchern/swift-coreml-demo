d6ec15ed8fddde6ffea999d231358309d292e2c1
Ru Chern Chong <ruchern@sproud.biz>
2017-10-09 16:19:31 +0800

    Complete UI
    Signed-off-by: Ru Chern Chong <ruchern@sproud.biz>

--- Trade My Tesla/ViewController.swift
+++ Trade My Tesla/ViewController.swift
@@ -1,17 +1,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var model: UISegmentedControl!
+    @IBOutlet weak var upgrades: UISegmentedControl!
+    @IBOutlet weak var mileageLabel: UILabel!
+    @IBOutlet weak var mileage: UISlider!
+    @IBOutlet weak var condition: UISegmentedControl!
+    @IBOutlet weak var valuation: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func calculateValue() {
+        
+    }
+    
 }
-
